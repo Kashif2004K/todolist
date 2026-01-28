b1de136f1dfc04e0f4554fad9ba4eea23717a9b1
Kashif <blackwatch4321@gmail.com>
2026-01-28 10:48:33 +0500

    changes

--- app.js
+++ app.js
@@ -3,15 +3,19 @@ const ul = document.querySelector("ul");
 const input = document.querySelector(".input");
 
 const addToDo = () => {
-  const li = document.createElement("li");
-  const delBtn = document.createElement("button");
+  li = document.createElement("li");
+  delBtn = document.createElement("button");
+  editBtn = document.createElement("button");
   const div = document.createElement("div");
   div.classList.add("todo-item");
   delBtn.classList.add("delete-btn");
+  editBtn.classList.add("edit-btn");
   delBtn.textContent = "Delete";
+  editBtn.textContent = "Edit";
   li.textContent = input.value;
   div.appendChild(li);
   div.appendChild(delBtn);
+  div.appendChild(editBtn);
   ul.appendChild(div);
   input.value = "";
 };
@@ -23,5 +27,21 @@ const deleteToDo = (e) => {
   }
 };
 
+const editToDo = (e) => {
+  if (e.target.classList.contains("edit-btn")) {
+    const editInput = document.createElement("input");
+    delBtn.style.display = "none";
+    editBtn.style.display = "none";
+    const confirmBtn = document.createElement("button");
+    confirmBtn.classList.add("confirm-button");
+    confirmBtn.textContent = "Confirm";
+    const editDiv = document.createElement("div");
+    editDiv.appendChild(editInput);
+    editDiv.appendChild(confirmBtn);
+    li.replaceWith(editDiv);
+  }
+};
+
+ul.addEventListener("click", editToDo);
 ul.addEventListener("click", deleteToDo);
 btn.addEventListener("click", addToDo);
